3068ecad67de56c264c7651969468c5c56b92655
yash6rana6 <yash91471@gmail.com>
2026-03-07 22:37:55 +0530

    fix useSearchParams build error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import LoginClient from "./LoginClient";
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return (
     <Suspense
