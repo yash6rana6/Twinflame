1e621ab6769cdd0ef3c1b5a7dfc3d540068bd8e8
White Night <yash91471@gmail.com>
2026-07-14 12:16:03 +0530

    fix layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -73,6 +73,7 @@ export const viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
+  interactiveWidget: "resizes-content",
 };
 
 export default function RootLayout({ children }) {
